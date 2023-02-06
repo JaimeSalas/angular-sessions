d7a8890d038b7ab18f093a842c845a715bf9290e
jaimesalas <jaime.salas@lemoncode.net>
2023-02-06 17:41:04 +0100

    added basic menu

--- pets/src/app/app.component.ts
+++ pets/src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``,
-  styles: [``]
+  template: `
+    <ul class="menu">
+      <li>
+        <a [routerLink]="['/welcome']">Home</a>
+      </li>
+      <li>
+        <a [routerLink]="['/pets']">Pets</a>
+      </li>
+    </ul>
+    <router-outlet></router-outlet>
+  `,
+  styles: [`
+    .menu {
+      display: flex;
+      justify-content: space-between;
+    }
+    .menu li {
+      list-style-type: none;
+    }
+  `]
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
