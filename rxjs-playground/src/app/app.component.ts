5d080c1ee94f647a2c7d4ac2bf7ed531dd0665ed
jaimesalas <jaime.salas@lemoncode.net>
2023-02-08 16:12:30 +0100

    added LCS

--- rxjs-playground/src/app/app.component.ts
+++ rxjs-playground/src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of, from, interval } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
+
+// pipe(map(fn))
+function myMap(fn: Function) {
+  return (input: Observable<any>) =>
+    new Observable((observer) => {
+      // observer.next(input)
+      return input.subscribe({
+        next: value => observer.next(fn(value)),
+        error: (err) => observer.error(err),
+        complete: () => observer.complete()
+      })
+    })
+}
 
 @Component({
   selector: 'app-root',
@@ -8,10 +22,35 @@ import { Observable, of, from, interval } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    const beerStream = of(['Beer 1', 'Beer 2']).subscribe(console.log);
-    // const beerStream = of('Beer 1', 'Beer 2').subscribe(console.log);
-    // const beerStream = from(['Beer1', 'Beer2']).subscribe(console.log);
-    // interval(1000).subscribe(console.log);
+    // of(1, 2, 3)
+    //   .pipe(map((i) => i * 2))
+    //   .subscribe(console.log);
+    // of(2, 4, 6).pipe(take(2)).subscribe(console.log);
+    // of(2, 4, 6)
+    //   .pipe(
+    //     tap(console.log),
+    //     map(i => i * 2),
+    //     take(2),
+    //     tap(console.log),
+    //     map(i => i - 3)
+    //   )
+    // Streams
+    from([5, 5, 10, 5]).pipe(
+      tap(console.log),
+      myMap((i: number) => i * 2),
+      map((i) => i as unknown as number - 10),
+      map((i) => {
+        if (i === 0) {
+          throw new Error('zero detected');
+        }
+        return i;
+      }),
+      take(3)
+    ).subscribe({
+      next: i => console.log(`item ${i}`),
+      error: err => console.log(`error occured ${err}`),
+      complete: () => console.log('complete')
+    });
   }
   title = 'rxjs-playground';
 }
